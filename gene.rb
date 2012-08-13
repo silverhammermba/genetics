898618edb0af315fe00ecd8676ebcb769fe095b0
Max <silverhammermba@gmail.com>
2012-08-12 21:28:23 -0400

    Correct Allele spelling. Remove mutation tracking

--- gene.rb
+++ gene.rb
@@ -1,45 +1,4 @@
-class Gene
-	@@mutation_rate = 0.1
-
-	def self.sample
-		new *Array.new(2) { Alele::pool.sample }
-	end
-
-	def initialize a1, a2, mutated = false
-		@aleles = [a1, a2].sort_by(&:to_s)
-		@mutated = mutated
-	end
-
-	attr_reader :aleles, :mutated
-
-	def to_s
-		@aleles.join + (@mutated ? '*' : '')
-	end
-
-	def dominant?
-		@aleles.any? { |a| a.dominant }
-	end
-
-	def random_alele
-		# choose a random alele or mutate
-		if rand < @@mutation_rate
-			(Alele::pool - @aleles).sample
-		else
-			@aleles.sample
-		end
-	end
-
-	def + gene
-		# choose aleles
-		als = [random_alele, gene.random_alele]
-		# check for (apparent mutations)
-		mutation = als.any? { |a| [self, gene].all? { |g| not g.aleles.include?(a) } }
-
-		self.class.new(*als, mutation)
-	end
-end
-
-class Alele
+class Allele
 	def initialize name, dominant
 		@name = name
 		@dominant = dominant
@@ -57,4 +16,42 @@ class Alele
 	private_class_method :new
 
 	attr_reader :name, :dominant
+end
+
+class Gene
+	@@mutation_rate = 0.1
+
+	def self.sample
+		new *Array.new(2) { Allele::pool.sample }
+	end
+
+	def initialize a1, a2
+		@alleles = [a1, a2].sort_by(&:to_s)
+	end
+
+	attr_reader :alleles
+
+	def to_s
+		@alleles.join
+	end
+
+	def dominant?
+		@alleles.any? { |a| a.dominant }
+	end
+
+	def random_allele
+		# choose a random allele or mutate
+		if rand < @@mutation_rate
+			Allele::pool.sample
+		else
+			@alleles.sample
+		end
+	end
+
+	def + gene
+		# choose alleles
+		als = [random_allele, gene.random_allele]
+
+		self.class.new *als
+	end
 end
