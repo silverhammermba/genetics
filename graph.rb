b8ed6ac4abd5565bbfa5d7ee27cdfce72de74065
Max <silverhammermba@gmail.com>
2012-12-08 15:24:30 -0500

    Add basic graph methods

--- graph.rb
+++ graph.rb
@@ -1,7 +1,14 @@
 class Node
-	def initialize
+	def initialize contents
 		@edges = []
+		@contents = contents
 	end
+
+	def method_missing? meth, *args, &blk
+		@contents.send(meth, *args &blk)
+	end
+
+	attr_reader :edges
 end
 
 class Edge
@@ -11,10 +18,27 @@ class Edge
 		@node[b] = a
 	end
 
+	def nodes
+		@node.keys
+	end
+
 	def [] *args
 		@node[*args]
 	end
 end
 
 class Graph
+	def initialize
+		@nodes = []
+		@edges = []
+	end
+
+	attr_reader :nodes, :edges
+
+	def add node
+		e = []
+
+		@nodes << node
+
+	end
 end
